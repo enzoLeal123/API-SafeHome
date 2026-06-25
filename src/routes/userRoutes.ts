463026d8308e1f37a9af4b497e57012a70ca3a54
nickinn-ttw <drnickolascruz@gmail.com>
2026-06-24 22:29:03 -0300

    UserRoutesCorrigido

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,10 +1,25 @@
 import { Router } from 'express';
-import { handleListContatos, handleAddContato } from '../controllers/contatoController';
+import {
+    handleGetMyProfile,
+    handleGetMyStatus,
+    handleAddContact,
+    handleListContacts,
+    handleUpdateFcmToken,
+    handleUpdateProfile,
+    handleSearchUser,
+    handleDeleteContact
+} from '../controllers/userController';
 import { authMiddleware } from '../middlewares/authMiddleware';
 
-const router = Router();
+const userRouter = Router();
 
-router.get('/', authMiddleware, handleListContatos);
-router.post('/', authMiddleware, handleAddContato);
+userRouter.get('/me', authMiddleware, handleGetMyProfile);
+userRouter.patch('/me', authMiddleware, handleUpdateProfile);
+userRouter.get('/me/status', authMiddleware, handleGetMyStatus);
+userRouter.get('/search', authMiddleware, handleSearchUser);
+userRouter.patch('/fcm-token', authMiddleware, handleUpdateFcmToken);
+userRouter.post('/contact', authMiddleware, handleAddContact);
+userRouter.get('/contacts', authMiddleware, handleListContacts);
+userRouter.delete('/contact/:id_relacao', authMiddleware, handleDeleteContact);
 
-export default router;
+export default userRouter;
